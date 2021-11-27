a017147351c1b79cbdc81db45e0ca399a84041f0
simon <simon200093@163.com>
2021-11-27 14:08:50 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
-    baseURL: 'http://localhost:8888',
+    // baseURL: 'http://localhost:8888',
+    baseURL: 'https://mp-api.simonyc.tech',
     timeout: 5000
 });
 
